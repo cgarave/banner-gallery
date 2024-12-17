11ee9e1956626dc2e68c000b6cf7e592f79c77cf
Rave Flores <reyvflores40@gmail.com>
2024-12-18 01:18:49 +0800

    added select and remove image

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imageContainer = document.getElementById('image-container');
+const selectedContainer = document.getElementById('selectedImg-container');
 
 //for All images button
 const showAllImages = document.getElementById('all-images')
@@ -44,6 +45,31 @@ let imageClasses = {
 for(let i = 0; i <= imageClasses.images.length - 1; i++){
     let imageDiv = document.createElement('div');
     imageDiv.innerHTML = `<img class="${imageClasses.images[i][0]}" src="src/${imageClasses.images[i][1]}">`
-    imageDiv.classList.add('w-64', 'h-64', 'overflow-hidden', 'cursor-pointer', 'rounded-lg', 'bg-red-400')
+    imageDiv.classList.add('w-64', 'h-64', 'overflow-hidden', 'cursor-pointer', 'rounded-lg', 'bg-red-400', 'hover:border-2', 'hover:border-[#FF9200]')
     imageContainer.appendChild(imageDiv)
+
+    //add selected images to selected images container
+    imageDiv.addEventListener('click', (event) => {
+        let image = event.target.attributes[1].value
+        appendSelectedImages(image);
+        //console.log(event.target.attributes[1].value);  
+    })
 }
+
+function appendSelectedImages(selected){
+    const selectedDiv = document.createElement('div');
+    selectedDiv.innerHTML = `<div id="remove" class="group-hover:block absolute z-10 top-2 right-2 bg-black  rounded-full p-2 hidden cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg"  width="14"  height="14"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                            </div>
+                            <div class="group-hover:block absolute z-0 bg-white opacity-20 h-full w-full hidden"></div>
+                            <img src="${selected}">`
+    selectedDiv.classList.add('group', 'rounded-lg', 'overflow-hidden', 'relative');
+    selectedContainer.appendChild(selectedDiv)
+
+    selectedDiv.addEventListener('click', (event) => {
+        if(event.target.id === 'remove' || event.target.parentElement.id === 'remove'){
+            selectedContainer.removeChild(selectedDiv)
+        }
+    })
+}
+
